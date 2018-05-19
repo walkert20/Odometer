5446de0cbd82385c8ec95450d080556da9dfc8ef
walkert <walkert20@hanover.edu>
2018-05-19 18:41:07 -0400

    Defined Binder and added location listener service.

---
+++ app/src/main/java/edu/hanover/odometer/OdometerService.java
@@ -0,0 +1,59 @@
+package edu.hanover.odometer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class OdometerService extends Service {
+    private static double distanceInMeters;
+    private static Location lastLocation = null;
+    private final IBinder binder = new OdometerBinder();
+
+    @Override
+    public void onCreate() {
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (lastLocation == null) {
+                    lastLocation = location;
+                }
+                distanceInMeters += location.distanceTo(lastLocation);
+                lastLocation = location;
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+    }
+
+    public class OdometerBinder extends Binder {
+        OdometerService getOdometer() {
+            return OdometerService.this;
+        }
+    }
+
+    public OdometerService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+}
