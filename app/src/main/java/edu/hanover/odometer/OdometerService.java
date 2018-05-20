ba81798037219970453160175d6960ebcfe371b9
walkert <walkert20@hanover.edu>
2018-05-20 16:24:32 -0400

    Binding to the app is now possible.

--- app/src/main/java/edu/hanover/odometer/OdometerService.java
+++ app/src/main/java/edu/hanover/odometer/OdometerService.java
@@ -1,18 +1,31 @@
 package edu.hanover.odometer;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Binder;
-import android.os.IBinder;
 
 public class OdometerService extends Service {
     private static double distanceInMeters;
     private static Location lastLocation = null;
     private final IBinder binder = new OdometerBinder();
+
+
+    public class OdometerBinder extends Binder {
+        OdometerService getOdometer() {
+            return OdometerService.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
 
     @Override
     public void onCreate() {
@@ -41,19 +54,11 @@ public class OdometerService extends Service {
 
             }
         };
+        LocationManager locManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, listener);
     }
 
-    public class OdometerBinder extends Binder {
-        OdometerService getOdometer() {
-            return OdometerService.this;
-        }
-    }
-
-    public OdometerService() {
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        return binder;
+    public double getMiles() {
+        return this.distanceInMeters / 1609.344;
     }
 }
